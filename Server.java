5e8facd7974d930e6756cf7f2cb185adb0d26931
hardforme <739683786@qq.com>
2018-04-24 12:42:04 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,4 +1,4 @@
- package server;
+package server;
 
 import java.util.Scanner;
 import java.io.*;
@@ -8,6 +8,7 @@ import java.sql.*;
 public class Server implements Runnable //让服务器变为线程体
 {
 	Socket s;
+	public static Connection dbConn = ConnectToDB() ;
 	public Server(Socket socket)
 	{
 		s = socket;
@@ -15,7 +16,6 @@ public class Server implements Runnable //让服务器变为线程体
 	public static void main(String[] args) 
 	{
 		  int MaxClientNum = 5;
-		 // ConnectToDB();
 	      try {
 	         ServerSocket server = new ServerSocket(8887);
 	         System.out.println("启动服务器....");
@@ -34,7 +34,7 @@ public class Server implements Runnable //让服务器变为线程体
 	{		
 		try {
 		System.out.println("客户端:"+s.getInetAddress().getLocalHost()+"已连接到服务器");
-        Thread t1 = new Thread(new GetCmsg(s));//接受客户端信息的线程
+        Thread t1 = new Thread(new GetCmsg(s,dbConn)); //接受客户端信息的线程
         Thread t2 = new Thread(new SendSmsg(s));//发送服务器信息的线程
         t1.start();
         t2.start();
@@ -44,7 +44,7 @@ public class Server implements Runnable //让服务器变为线程体
 		}
 	}
 	
-	public static void ConnectToDB()//连接到SQLserver数据库
+	public static Connection ConnectToDB() //连接到SQLserver数据库
 	{
 		String driverName = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
 		String dbURL = "jdbc:sqlserver://localhost:1433;DatabaseName=OO";
@@ -59,60 +59,70 @@ public class Server implements Runnable //让服务器变为线程体
 		}
 		
 		try{  
-		    Connection dbConn=DriverManager.getConnection(dbURL,userName,userPwd);  
+		        Connection dbConn=DriverManager.getConnection(dbURL,userName,userPwd);  
 		        System.out.println("连接数据库成功！");  
+		        return dbConn;
 		}catch(Exception e)  
 		{  
 		    e.printStackTrace();  
 		    System.out.print("SQL Server连接失败！");  
+		    return null;
 		} 
 	}
 }
 
-class Cmsg implements Serializable//序列化客户端信息，使socket可以传输对象
+class Cmsg implements Serializable //序列化客户端信息，使socket可以传输对象
 {
 	public char msg_type;
 	public String msg;
 	public String id;
-	public String psw;
+	public String pwd;
 	public Cmsg(char msg_type, String msg)
 	{
 		this.msg_type = msg_type;
 		this.msg = msg;
 	}
-	public Cmsg(char msg_type, String id,String psw)
+	public Cmsg(char msg_type, String id,String pwd)
 	{
 		this.msg_type = msg_type;
 		this.id = id;
-		this.psw = psw;
+		this.pwd = pwd;
 	}
 }
 
-class GetCmsg implements Runnable//接受客户端信息
+class GetCmsg implements Runnable //接受客户端信息
 {
 	Cmsg message;
 	Socket s;
-	public GetCmsg(Socket socket)//传入已建立的socket
+	Connection con;
+	public GetCmsg(Socket socket,Connection dbConn)//传入已建立的socket 和数据库
 	{
 		s = socket;
+		con = dbConn;
 	}
 	public void run()
 	{
 		try{
-				int n = 1;
+				 Statement stmt = con.createStatement();
+				 ResultSet rs;
+				 String sqlcmd;
 				 ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
-				 while((message = (Cmsg)ois.readObject()).msg_type != '0')//0代表用户退出
+				 while((message = (Cmsg)ois.readObject()).msg_type != '0') //0代表用户退出
 				 {
-					 System.out.println(n);
-					 if(message.msg_type == '1')//1代表登陆
-					 {
-						 System.out.println("Client " +s.getInetAddress().getLocalHost()+ ":"+message.id+message.psw);
+					 if(message.msg_type == '1') //1代表登陆
+					 {		
+						 sqlcmd = "select OPassWord from OOUser where OID = "+message.id;
+						 rs = stmt.executeQuery(sqlcmd);
+						 rs.next();
+						 String pwd = rs.getString("OPassWord");
+						 if(pwd.trim().equals(message.pwd)) System.out.println("Client " +s.getInetAddress().getLocalHost()+ ":登陆成功");
+						 else System.out.println("Client " +s.getInetAddress().getLocalHost()+ ":账号不存在或密码错误");
+						 //System.out.println("Client " +s.getInetAddress().getLocalHost()+ ":"+message.id+message.psw);
 					 }
 					 else 
 					 {
 		            	 System.out.println("Client " +s.getInetAddress().getLocalHost()+ ":"+message.msg);
 					 }
-					 n++;
 		         }
 			}catch(Exception e)
 			{
@@ -121,7 +131,7 @@ class GetCmsg implements Runnable//接受客户端信息
 	}
 }
 
-class SendSmsg implements Runnable//发送服务器信息
+class SendSmsg implements Runnable //发送服务器信息
 {
 	String Smsg;
 	Socket s;
